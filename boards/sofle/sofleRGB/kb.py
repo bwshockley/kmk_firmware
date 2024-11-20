bc286df1a156b1b406b6124d58062ace78118388
Benjamin Shockley <benjaminshockley@hotmail.com>
2024-11-19 18:34:27 -0600

    Update kb.py

--- boards/sofle/sofleRGB/kb.py
+++ boards/sofle/sofleRGB/kb.py
@@ -11,12 +11,16 @@ class KMKKeyboard(_KMKKeyboard):
     diode_orientation = DiodeOrientation.COL2ROW
     encoder_pin_0 = board.GP29
     encoder_pin_1 = board.GP28
+    rgb_pixel_pin = board.GP0
+    uart_pin = board.GP1
+    data_pin = board.GP1
+    i2c = board.I2C
     # fmt:off
     coord_mapping = [
          0,  1,  2,  3,  4,  5,    35, 34, 33, 32, 31, 30,
          6,  7,  8,  9, 10, 11,    41, 40, 39, 38, 37, 36,
         12, 13, 14, 15, 16, 17,    47, 46, 45, 44, 43, 42,
         18, 19, 20, 21, 22, 23,    53, 52, 51, 50, 49, 48,
-        24, 25, 26, 27, 28, 29,    59, 58, 57, 56, 55, 54
+        24, 25, 26, 27, 28, 29,    59, 58, 57, 56, 55, 54,
     ]
     # fmt:on
